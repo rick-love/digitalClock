fbaaa52cc289f7c106e91561f991be04f8e0200a
rick-love <ricklove@outlook.com>
2017-07-17 13:46:48 -0400

    uploaddingclock

---
+++ clock.js
@@ -0,0 +1,42 @@
+//Pseudo Code
+// create variables for time of day
+// create variable for day and month
+// create variable to concat time variables
+
+// create div with id for time;
+
+// connect time to be 
+
+
+
+
+function digitalClock(){
+	// var clockFace = document.createElement('div');
+	// var monthDay = document.createElement('div');
+	// clockFace.setAttribute('id', 'myClock');
+	// monthDay.setAttribute('id', 'monthDay');
+
+//gets current time
+	var date = new Date();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+	var month = date.getMonth();
+	var day = date.getDay();
+
+//concats time and date variables
+	var currentTime = '#'+ hour + minute + second;
+	
+
+// populates HTML 'div's'
+	document.getElementById('clock').innerHTML = currentTime;
+	
+// sets the refresh
+	setInterval(digitalClock, 500);
+
+// sets background color
+	document.bgColor = currentTime;
+
+
+}
+digitalClock();
